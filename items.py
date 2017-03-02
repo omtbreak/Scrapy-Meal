792234740bc519c3d8bb1f54648d4d07a14c5c88
omtbreak <omtbreak@gmail.com>
2017-03-02 19:38:01 +0800

    Update items.py

--- items.py
+++ items.py
@@ -6,29 +6,49 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy import Item
-
-class MenuItem(scrapy.Item):
-    name=scrapy.Field()
-    href=scrapy.Field()
-
-
-class MealPageItem(scrapy.Item):
-    category=scrapy.Field()#菜属于哪个类别
-    mealName=scrapy.Field()#菜的名字
-    times=scrapy.Field()#有多少人做过
-    authorName=scrapy.Field()#作者
-    mealHref=scrapy.Field()#菜的链接
-    authorHref=scrapy.Field()#作者链接
 
 
 
 class MealItem(scrapy.Item):
-    photo=scrapy.Field()
+    #1,照片链接
+    photoUrl=scrapy.Field()
+    #2，菜的评分
     score=scrapy.Field()
+    #3，有多少人做过
     times=scrapy.Field()
-    #category=scrapy.Field()
-    #author=scrapy.Field()
+    #4，菜的类别
+    category=scrapy.Field()
+    #5，作者链接
+    authorUrl=scrapy.Field()
+    #6，作者名字
+    authorName=scrapy.Field()
+    #7，菜的简介
     description=scrapy.Field()
+    #8，菜的材料
     ingredients=scrapy.Field()
+    #9，做菜步骤
     procedure=scrapy.Field()
+
+class AuthorItem(scrapy.Field):
+    #1, 作者名字
+    authorTitle=scrapy.Field()
+    #2, 作者性别
+    authorSexuality=scrapy.Field()
+    #3，作者城市定位
+    authorLocation=scrapy.Field()
+    #4，作者工作
+    authorJob=scrapy.Field()
+    #5，加入下厨房日期
+    authorJoinDate=scrapy.Field()
+    #6，作者新浪微博URL
+    #authorWeiBoUrl=scrapy.Field()
+    #7，作者简介
+    authorDescription=scrapy.Field()
+    #8，作者关注的用户数量
+    authorFollowing=scrapy.Field()
+    #9，关注作者的用户数量
+    authorFollower=scrapy.Field()
+    #10，作者头像URL
+    authorImageUrl=scrapy.Field()
+
+
